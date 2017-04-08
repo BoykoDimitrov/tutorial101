cb13ea60e7689a9a82d9354a4690ffc25f5a3472
batboy4e <boyko.dim@gmail.com>
2017-04-08 16:27:59 +0300

    Add OrgHello package

--- src/OrgHello/HelloWorld.java
+++ src/OrgHello/HelloWorld.java
@@ -1,7 +1,7 @@
-package tutorial101;
+package OrgHello;
 
 public class HelloWorld {
 	public static void main(String[] args) {
-		System.out.println("Hello World");	
+		System.out.println("Hello Java World");
 	}
 }
